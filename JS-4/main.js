18d3fad11a9a695490c00ddc52a86a4af5b5b534
Ranjit Kairi <ranjitkoiri009@gmail.com>
2022-02-05 22:04:19 +0530

    js-4

---
+++ JS-4/main.js
@@ -0,0 +1,8 @@
+//numbers
+// An integer is a whole number - without a fractional part.
+const myNumber = 42;
+// A number with a decimal point is a float, with reference the "floating" point.
+const myFloat = 42.42;
+
+const myString = "42";
+console.log(Number(myString) + myNumber);
